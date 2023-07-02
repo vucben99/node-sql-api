5e8eaf06693ab734fec4fde4dc03c235cb29ed91
Beni Vucsics <vucben99@gmail.com>
2023-07-02 20:22:18 +0200

    Added export for PrismaClient

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import { PrismaClient } from '@prisma/client'
 
 import articles from './routes/articles'
 import token from './routes/token'
 
 const PORT = process.env.PORT || 3000
+export const prisma = new PrismaClient()
 const app = express()
 
 // Middleware
