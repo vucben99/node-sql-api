0589b026673735dcb69d1ea1f5336622d9af3115
Beni Vucsics <vucben99@gmail.com>
2023-07-03 21:04:05 +0200

    Added Swagger

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import { PrismaClient } from '@prisma/client'
+import swaggerDoc from './swagger'
 
 import articles from './routes/articles'
 import token from './routes/token'
 
-
+// Config
 const PORT = process.env.PORT || 3000
 export const prisma = new PrismaClient()
 const app = express()
@@ -20,6 +21,10 @@ app.use(morgan("dev"))
 app.use('/articles', articles)
 app.use('/token', token)
 
+// Swagger
+swaggerDoc(app)
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
