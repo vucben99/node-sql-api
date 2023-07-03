08d421c77f2564234d5d811f9cd7a3418496db64
Beni Vucsics <vucben99@gmail.com>
2023-07-03 11:19:12 +0200

    Rmvd non-necessary try-catch blocks + minor tweaks

--- routes/token.ts
+++ routes/token.ts
@@ -1,66 +1,56 @@
 import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
+import { z } from 'zod'
 
 import { prisma } from '../index'
-import validateNewTokenRequest from '../middleware/validateNewTokenRequest'
+import { NewTokenRequestSchema, NewTokenRequest, RenewTokenRequestSchema, RenewTokenRequest } from '../Schemas'
+import validateRequestSchema from '../middleware/validateRequestSchema'
 
 const router = express.Router()
 
-type TokenRequest = {
-  platform: string
-}
-
-router.post('/', validateNewTokenRequest, async (req, res) => {
-  const { platform } = req.body as TokenRequest
-  try {
-    const session = await prisma.token.create({
-      data: {
-        token: uuidv4(),
-        platform
-      }
-    })
-    return res.json({
-      token: session.token,
-      remaining: session.remaining
-    })
-  } catch (error) {
-    console.error(error)
-    return res.sendStatus(500)
-  }
+// /token endpoint
+router.post('/', validateRequestSchema(NewTokenRequestSchema), async (req, res) => {
+  const { platform } = req.body as NewTokenRequest
+  if (!platform) return res.status(400).json({
+    error: 'Please provide a platform! (WEB | IOS | ANDROID)'
+  })
+  const session = await prisma.token.create({
+    data: {
+      token: uuidv4(),
+      platform
+    }
+  })
+  if (!session) return res.sendStatus(500)
+  return res.json({
+    token: session.token,
+    remaining: session.remaining
+  })
 })
 
-type RenewTokenRequest = {
-  token: string
-}
-
-router.post('/renew', async (req, res) => {
+// /token/renew endpoint
+router.post('/renew', validateRequestSchema(RenewTokenRequestSchema), async (req, res) => {
   const { token } = req.body as RenewTokenRequest
-  if(!token) return res.status(400).json({
+  if (!token) return res.status(400).json({
     error: 'Missing token'
   })
 
-  try {
-    const session = await prisma.token.update({
-      where: {
-        token
-      },
-      data: {
-        remaining: 5
-      }
-    })
+  const session = await prisma.token.update({
+    where: {
+      token
+    },
+    data: {
+      remaining: 5
+    }
+  })
 
-    if (!session) return res.status(403).json({
-      error: 'Not a valid token'
-    })
+  if (!session) return res.status(403).json({
+    error: 'Not a valid token'
+  })
 
-    return res.json({
-      token: session.token,
-      remaining: session.remaining
-    })
-  } catch (error) {
-    console.error(error)
-    return res.sendStatus(500)
-  }
+  return res.json({
+    token: session.token,
+    remaining: session.remaining
+  })
 })
 
 export default router
